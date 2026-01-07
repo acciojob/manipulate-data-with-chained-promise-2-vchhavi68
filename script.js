1d3803f5769611cf733027f557157be375323572
vchhavi68 <vchhavi68@gmail.com>
2026-01-07 13:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+//your JS code here. If required.function manipulateArray() {
+  const output = document.getElementById("output");
+
+
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+})
+  
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evens = arr.filter(num => num % 2 === 0);
+        output.textContent = evens.join(",");
+        resolve(evens);
+      }, 1000);
+    });
+  })
+
+  
+  .then((evens) => {
+    return new Promise(() => {
+      setTimeout(() => {
+        const doubled = evens.map(num => num * 2);
+        output.textContent = doubled.join(",");
+      }, 2000);
+    });
+  });
+
+  
+  
+
+
